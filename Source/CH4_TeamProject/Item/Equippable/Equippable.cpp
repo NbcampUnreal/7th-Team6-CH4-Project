4940a9d7689ef6513c05d876e8d4b5150a329360
yahoo-afk <sjffhdfg@gmail.com>
2026-04-03 19:16:24 +0900

    add.Item

---
+++ Source/CH4_TeamProject/Item/Equippable/Equippable.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Equippable.h"
+
+#include "GameFramework/Character.h"
+
+
+// Sets default values
+AEquippable::AEquippable()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEquippable::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEquippable::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEquippable::Equip(ACharacter* TargetCharacter)
+{
+	if (TargetCharacter && HasAuthority())
+	{
+		// 1. 소유자 설정 (이전에 했던 Owner 설정!)
+		SetOwner(TargetCharacter);
+
+		// 2. 물리 시뮬레이션 등이 있다면 끄기
+		// Mesh->SetSimulatePhysics(false);
+
+		// 3. 캐릭터 소켓에 부착
+		AttachToComponent(
+			TargetCharacter->GetMesh(), 
+			FAttachmentTransformRules::SnapToTargetIncludingScale
+			//
+		);
+        
+		UE_LOG(LogTemp, Warning, TEXT("%s 장착 완료!"), *GetName());
+	}
+}
+
+
---
+++ Source/CH4_TeamProject/Item/Equippable/Ranged Weapon/RangedWeapons.h
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RangedWeaponDataAsset.h"
+#include "../Equippable.h"
+#include "RangedWeapons.generated.h"
+
+UCLASS()
+class CH4_TEAMPROJECT_API ARangedWeapons : public AEquippable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	
+	ARangedWeapons();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	//virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_ApplyDamageToTarget(AActor* TargetActor);
+	
+	void ProcessDamage(AActor* TargetActor);
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Mesh")
+	TObjectPtr<UStaticMeshComponent> WeaponMesh;
+
+	int32 GetCurrentAmmo ()const
+	{
+		return CurrentAmmo; 
+	} 
+	
+	int32 GetMaxAmmo () const
+	{
+		if (DataAsset)
+		{
+			return DataAsset->MaxAmmo;
+		}
+		return 0;
+	}
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(Replicated)
+	int32 CurrentAmmo;
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_Fire();
+	
+	UFUNCTION(NetMulticast, Unreliable)
+	void Multicast_PlayEffects();
+
+	void TraceShoot();
+	
+	UFUNCTION(BlueprintCallable)
+	virtual void Fire();
+	
+	UPROPERTY(Replicated)
+	TObjectPtr<URangedWeaponDataAsset> DataAsset;
+	
+};
