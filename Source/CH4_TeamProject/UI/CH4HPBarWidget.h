c45ad42962fe9de85c46c2531db873fb71064ab9
justis0419 <justis0419@gmail.com>
2026-04-03 19:22:26 +0900

    Player, UI, Interface 클래스 추가

---
+++ Source/CH4_TeamProject/UI/CH4HPBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CH4HPBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH4_TEAMPROJECT_API UCH4HPBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CH4_TeamProject/UI/CH4HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CH4HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH4_TEAMPROJECT_API ACH4HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
