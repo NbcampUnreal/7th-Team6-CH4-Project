192b4e9cfd33bcaa3496460ad591e79df57a09f4
wksjdb87-design <wksjdb87@gmail.com>
2026-04-10 13:48:02 +0900

    [fix] UI 수정

--- Source/CH4_TeamProject/Player/CH4PlayerController.cpp
+++ Source/CH4_TeamProject/Player/CH4PlayerController.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #include "CH4PlayerController.h"
 #include "Blueprint/UserWidget.h"
 
@@ -9,16 +9,117 @@ ACH4PlayerController::ACH4PlayerController()
 
 void ACH4PlayerController::BeginPlay()
 {
-    // HUD 위젯 생성 및 표시
-    if (HUDWidgetClass)
+    if (!IsLocalController())
     {
-        UUserWidget* HUDWidget = CreateWidget<UUserWidget>(this, HUDWidgetClass);
-        if (HUDWidget)
-        {
-            HUDWidget->AddToViewport();
-        }
+        return;
+    }
+    
+    // TODO: 시작 시 어떤 메뉴를 먼저 띄울지 함수 호출 위치 결정
+    ShowStartMenu();
+}
+
+void ACH4PlayerController::BeginPlayingState()
+{
+    Super::BeginPlayingState();
+
+}
+
+void ACH4PlayerController::ShowStartMenu()
+{
+    HideCurrentWidget(); // 기존에 떠 있는 게 있다면 지웁니다.
+
+    if (!StartMenuClass) return;
+
+    // 1. 위젯 생성 (설계도인 Class로 실체인 Instance를 만듭니다)
+    CurrentWidget = CreateWidget<UUserWidget>(this, StartMenuClass);
+
+    if (CurrentWidget)
+    {
+        // 2. 화면에 붙이기
+        CurrentWidget->AddToViewport();
+
+        // 3. 마우스 설정: 시작 메뉴에서는 버튼을 눌러야 하니 마우스를 보여줍니다.
+        bShowMouseCursor = true;
+
+        // 4. 입력 모드: 게임 캐릭터 조작은 막고 UI만 만지게 설정합니다.
+        FInputModeUIOnly InputMode;
+        SetInputMode(InputMode);
     }
 }
+
+void ACH4PlayerController::ShowGameOver()
+{
+}
+
+void ACH4PlayerController::HideCurrentWidget()
+{
+    // 1. 만약 현재 떠 있는 메뉴(CurrentMenuWidget)가 있다면?
+    if (CurrentWidget)
+    {
+        // 화면에서 지우고
+        CurrentWidget->RemoveFromParent();
+
+        // 메모리 주소도 깨끗하게 비웁니다 (방금 배운 nullptr!)
+        CurrentWidget = nullptr;
+    }
+
+    // 3. 입력을 다시 '게임 전용'으로 바꿉니다. (그래야 캐릭터가 움직입니다)
+    FInputModeGameOnly InputMode;
+    SetInputMode(InputMode);
+}
+
+void ACH4PlayerController::StartGame()
+{
+    UGameViewportClient* Viewport = GetWorld()->GetGameViewport();
+    if (Viewport)
+    {
+        Viewport->RemoveAllViewportWidgets();
+    }
+
+    // 2. 현재 메뉴 변수 정리 (nullptr 방금 배운 거 활용!)
+    CurrentWidget = nullptr;
+    
+    // 3. 게임 화면이 나왔으니 다시 HUD(체력바 등)를 띄워줌
+    CurrentWidget = CreateWidget<UUserWidget>(this, HUDWidgetClass);
+    if (HUDWidgetClass)
+    {
+        CurrentWidget->AddToViewport();
+        
+        // 4. ★핵심: 입력 모드를 게임 전용으로! (이걸 안 하면 화면만 비고 조작이 안 됨)
+        FInputModeGameOnly InputMode;
+        SetInputMode(InputMode);
+        bShowMouseCursor = false;
+    }
+}
+
+void ACH4PlayerController::ExitGame()
+{
+    // UKismetSystemLibrary::QuitGame(월드 context, 컨트롤러, 종료 사유, 배경 실행 여부)
+    UE_LOG(LogTemp, Warning, TEXT("=== C++ ExitGame 진입 성공! ==="));
+    UKismetSystemLibrary::QuitGame(GetWorld(), this, EQuitPreference::Quit, false);
+}
+
+void ACH4PlayerController::ShowGameRule()
+{
+// 1. 이미 열린 게 있다면 지웁니다.
+    HideCurrentWidget();
+
+    // 2. 클래스 체크
+    if (!GameRulesWidgetClass) return;
+
+    // 3. 룰 위젯 생성 및 저장
+    CurrentWidget = CreateWidget<UUserWidget>(this, GameRulesWidgetClass);
+    if (CurrentWidget)
+    {
+        // 4. 화면에 띄우기 (AddToViewport)
+        CurrentWidget->AddToViewport();
+        
+        FInputModeUIOnly InputMode;
+        SetInputMode(InputMode);
+        bShowMouseCursor = true;
+    }
+}
+
 
 void ACH4PlayerController::Client_DisablePlayerInput_Implementation()
 {    
@@ -57,22 +158,54 @@ void ACH4PlayerController::Client_InvokeDownUI_Implementation()
     // 회색 화면 등 -> 구현 필요
 }
 
-void ACH4PlayerController::Client_HideDownUI_Implementation()
-{    
-    // 다운 UI 제거
-}
-
 void ACH4PlayerController::Client_MoveToLobby_Implementation() const
 {
     // 로비 UI로 이동
 }
 
+void ACH4PlayerController::Client_InvokeGameClearUI_Implementation()
+{
+    HideCurrentWidget();
+    
+    CurrentWidget = CreateWidget<UUserWidget>(this,HUDGameClearWidgetClass);
+    if (CurrentWidget)
+    {
+        CurrentWidget->AddToViewport();
+    }
+}
+
 void ACH4PlayerController::Client_InvokeGameClearUI_Implementation() const
 {
-    // 클리어 UI 띄우기
+    HideCurrentWidget();
+    
+    CurrentWidget = CreateWidget<UUserWidget>(this,HUDGameLoseWidgetClass);
+    {
+        CurrentWidget->AddToViewport();
+    }
 }
 
 void ACH4PlayerController::Client_InvokeGameLoseUI_Implementation() const
 {
-    // 패배 UI 띄우기
+    if (!IsLocalController())
+        return;
+    
+    if (bShow)
+    {
+        if (!HUDPlayerDownedWidgetInstance)
+        {
+            HUDPlayerDownedWidgetInstance = CreateWidget<UUserWidget>(this,HUDPlayerDownedWidgetClass);
+            if (HUDPlayerDownedWidgetInstance)
+            {
+                HUDPlayerDownedWidgetInstance->AddToViewport();
+            }
+        }
+    }
+    else
+    {
+        if (HUDPlayerDownedWidgetInstance)
+        {
+            HUDPlayerDownedWidgetInstance->RemoveFromParent();
+            HUDPlayerDownedWidgetInstance = nullptr;
+        }
+    }
 }
--- Source/CH4_TeamProject/Player/CH4PlayerController.h
+++ Source/CH4_TeamProject/Player/CH4PlayerController.h
@@ -17,30 +17,87 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	TSubclassOf<UUserWidget> HUDWidgetClass;
+	
+	UPROPERTY(EditAnywhere, Category = "MyUI")
+	TSubclassOf<UUserWidget> StartMenuClass; // 시작 화면용 주머니
+
+	// 룰 화면 위젯 블루프린트 클래스를 에디터에서 할당받습니다.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UUserWidget> GameRulesWidgetClass;
+
+	// 현재 열려 있는 룰 위젯의 인스턴스를 저장합니다. (nullptr 필수!)
+	UPROPERTY()
+	UUserWidget* CurrentRulesWidget = nullptr;
+	
+	//아이템 완성시 주석해제
+	//UPROPERTY(BlueprintReadOnly, Category = "GameData")
+	//int32 GamePhase;
+
+	//UPROPERTY(BlueprintReadOnly, Category = "GameData")
+	//int32 GearPartsCount;
+
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void ShowStartMenu();
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void ShowGameRule();
+
+	UFUNCTION(BlueprintCallable)
+	void ShowGameOver();
+
+	UFUNCTION(BlueprintCallable)
+	void HideCurrentWidget();
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void StartGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void ExitGame();
 	
 public:
-	//----합칠 수 있을듯----(bool 매개변수를 통해서)
+	UPROPERTY()
+	UUserWidget* CurrentWidget;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My UI")
+	TSubclassOf<UUserWidget> HUDLobbyWidgetClass;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "My UI")
+	UUserWidget* HUDLobbyWidgetInstance;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My UI")
+	TSubclassOf<UUserWidget> HUDGameClearWidgetClass;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "My UI")
+	UUserWidget* HUDGameClearWidgetInstance;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My UI")
+	TSubclassOf<UUserWidget> HUDGameLoseWidgetClass;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "My UI")
+	UUserWidget* HUDGameLoseWidgetInstance;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My UI")
+	TSubclassOf<UUserWidget> HUDPlayerDownedWidgetClass;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "My UI")
+	UUserWidget* HUDPlayerDownedWidgetInstance;
+	
 	UFUNCTION(Client, Reliable)
 	void Client_DisablePlayerInput();
 	
 	UFUNCTION(Client, Reliable)
 	void Client_EnablePlayerInput();
-	//---------------------------
 	
-	
-	//----합칠 수 있을듯----(EPlayerLifeState 매개변수에 따라서)
 	UFUNCTION(Client, Reliable)
 	void Client_PlayDownAnim();
 	
 	UFUNCTION(Client, Reliable)
 	void Client_PlayReviveAnim();
-	//---------------------------
-	
-	
-	//----합칠 수 있을듯----
+
 	UFUNCTION(Client, Reliable)
 	void Client_InvokeDownUI() ;
 	
