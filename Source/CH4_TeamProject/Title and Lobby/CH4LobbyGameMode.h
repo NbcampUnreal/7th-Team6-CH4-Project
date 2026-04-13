62e610c8fb5f29b3c458d1efd3c4c860221dba23
SEOWOOJEONG <unha1@naver.com>
2026-04-13 20:10:24 +0900

    add : LobbyGameMode Class

---
+++ Source/CH4_TeamProject/Title and Lobby/CH4LobbyGameMode.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "CH4LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH4_TEAMPROJECT_API ACH4LobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
