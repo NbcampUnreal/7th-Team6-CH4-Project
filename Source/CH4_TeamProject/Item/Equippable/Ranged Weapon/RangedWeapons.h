a95799d227dc3e4a312e183ba60716c33e334c57
wksjdb87-design <wksjdb87@gmail.com>
2026-04-17 17:14:44 +0900

    [Fix] 오류 수정

--- Source/CH4_TeamProject/Item/Equippable/Ranged Weapon/RangedWeapons.h
+++ Source/CH4_TeamProject/Item/Equippable/Ranged Weapon/RangedWeapons.h
@@ -17,67 +17,58 @@ public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere,Category="DataComponent")
 	TObjectPtr<class UEquippableComponent> WeaponComponent;
 	
-	ARangedWeapons();
-	
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-	
-	//virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
-	
-	UFUNCTION(Server, Reliable, WithValidation)
-	void Server_ApplyDamageToTarget(AActor* TargetActor);
-	
-	void ProcessDamage(AActor* TargetActor);
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Mesh")
-	TObjectPtr<UStaticMeshComponent> WeaponMesh;
-	
 	UPROPERTY(VisibleAnywhere, Category = "AI")
 	class UAIPerceptionStimuliSourceComponent* StimuliSource;
-
-	int32 GetCurrentAmmo ()const
-	{
-		return CurrentAmmo; 
-	} 
-	
-	int32 GetMaxAmmo () const;
-	
+    
+	ARangedWeapons();
+    
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+    
+	//virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+    
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_ApplyDamageToTarget(AActor* TargetActor);
+    
+	void ProcessDamage(AActor* TargetActor);
+    
 	virtual void Tick(float DeltaTime) override;
-	
+    
 	UFUNCTION()
 	virtual void Attack_Implementation() ;
-	
+    
 	virtual void Attack() ;
 	virtual void Attack_Implementation_Internal() override;
-	
-	
+    
+    
 
 	virtual void Reload_Implementation_Internal() override;
-	
+    
 	UPROPERTY(ReplicatedUsing = OnRep_FireReady)
 	bool bIsCoolingDown= false;
-	
+    
 	virtual void AddMaxClip(int32 AmmoItem )override;
-	
+    
 
-	
-protected:	
+    
+protected:  
 	virtual void BeginPlay() override;
-	
-	
-	
+    
+    
+    
 	UFUNCTION(NetMulticast, Unreliable)
 	void Multicast_PlayEffects(FVector TraceStart, FVector TraceEnd, bool bHit);
-	
+    
 	void TraceShoot();
 
 	virtual int32 GetMaxAmmo()const override;
 	virtual int32 GetMaxClip() const override;
-	
+    
 	void ProcessReload();
-	
-	
+    
+    
 	FTimerHandle TimerHandle_FireDelay;
 	UFUNCTION()
 	void OnRep_FireReady(); 
-	
+    
 	void ResetCoolTime();
 };
