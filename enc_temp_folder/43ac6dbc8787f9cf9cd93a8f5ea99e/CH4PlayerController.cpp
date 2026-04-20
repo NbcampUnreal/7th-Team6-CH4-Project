fe27255074b7b13eeeab8f2074b911c8c4b5d2dd
justis0419 <justis0419@gmail.com>
2026-04-20 17:08:25 +0900

    crosshair, 캐릭터 HUD 설정

--- enc_temp_folder/43ac6dbc8787f9cf9cd93a8f5ea99e/CH4PlayerController.cpp
+++ enc_temp_folder/43ac6dbc8787f9cf9cd93a8f5ea99e/CH4PlayerController.cpp
@@ -25,8 +25,6 @@ void ACH4PlayerController::BeginPlay()
 		FInputModeGameOnly InputMode;
 		SetInputMode(InputMode);
 		bShowMouseCursor = false;
-
-		StartGame();
 	}
 	
 	// TODO: 시작 시 어떤 메뉴를 먼저 띄울지 함수 호출 위치 결정
